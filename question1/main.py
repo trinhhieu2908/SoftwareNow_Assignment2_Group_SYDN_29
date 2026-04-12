a0005e13cd65c5ea9471801b8a68487c4d258548
trinhhieu <trantrinhhieu2908@gmail.com>
2026-04-12 23:02:43 +1000

    implement verification function

--- question1/main.py
+++ question1/main.py
@@ -2,6 +2,21 @@ from encrypt import encrypt
 from decrypt import decrypt
 
 
+def verify():
+    f = open("raw_text.txt", "r", encoding="utf-8")
+    raw_text = f.read()
+    f.close()
+
+    f = open("decrypted_text.txt", "r", encoding="utf-8")
+    decrypted_text = f.read()
+    f.close()
+
+    if raw_text == decrypted_text:
+        print("The decryption was successful.")
+    else:
+        print("The decryption was not successful.")
+
+
 def main():
     shift1 = int(input("Enter shift1: "))
     shift2 = int(input("Enter shift2: "))
@@ -21,6 +36,8 @@ def main():
     f = open("decrypted_text.txt", "w", encoding="utf-8")
     f.write(decrypted)
     f.close()
+
+    verify()
 
 
 if __name__ == "__main__":
