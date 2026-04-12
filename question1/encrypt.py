b8a923f8f6e279f5cedf259562b52e290be4e943
trinhhieu <trantrinhhieu2908@gmail.com>
2026-04-12 22:46:00 +1000

    logic for encrypt function

--- question1/encrypt.py
+++ question1/encrypt.py
@@ -1,4 +1,24 @@
 def encrypt(text, shift1, shift2):
-    # TODO: real encryption rules later
-    _ = (shift1, shift2)
-    return text
+    result = ""
+    for character in text:
+        # lowercase
+        if "a" <= character <= "m":
+            position = ord(character) - ord("a")
+            new_position = (position + shift1 * shift2) % 26
+            result += chr(ord("a") + new_position)
+        elif "n" <= character <= "z":
+            position = ord(character) - ord("a")
+            new_position = (position - (shift1 + shift2)) % 26
+            result += chr(ord("a") + new_position)
+        # uppercase
+        elif "A" <= character <= "M":
+            position = ord(character) - ord("A")
+            new_position = (position - shift1) % 26
+            result += chr(ord("A") + new_position)
+        elif "N" <= character <= "Z":
+            position = ord(character) - ord("A")
+            new_position = (position + shift2 * shift2) % 26
+            result += chr(ord("A") + new_position)
+        else:
+            result += character
+    return result
